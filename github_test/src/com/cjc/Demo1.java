09693d0c7a551a8088de49b601dfda36f2f96ab3
Purva1010 <107473683+Purva1010@users.noreply.github.com>
2022-06-15 15:07:19 +0530

    MethodAdd

--- github_test/src/com/cjc/Demo1.java
+++ github_test/src/com/cjc/Demo1.java
@@ -2,8 +2,13 @@ package com.cjc;
 
 public class Demo1
 {
+	public void m1()
+	{
+	  System.out.println("M1 Method of git hub project");
+	}
  public static void main(String[] args) 
  {
 	System.out.println("This is git hub project");
 }
+	
 }
