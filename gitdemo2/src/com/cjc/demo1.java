06bbec4e89f69056ed3f1833774e5b7bff2952a4
Purva1010 <107473683+Purva1010@users.noreply.github.com>
2022-06-20 15:43:25 +0530

    by purva

--- gitdemo2/src/com/cjc/demo1.java
+++ gitdemo2/src/com/cjc/demo1.java
@@ -2,6 +2,10 @@ package com.cjc;
 
 public class demo1 
 {
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
  public static void main(String[] args) 
  {
 	System.out.println("Hellloo");
